ccca4ef882b717a6477012ad8ded0113420ca96a
Phi Vu Nguyen <phi@marpipe.com>
2022-09-29 12:28:04 -0400

    Add logs.

--- tests-war/src/main/java/org/nirkoren/maven/demo/TomcatPropsHandler.java
+++ tests-war/src/main/java/org/nirkoren/maven/demo/TomcatPropsHandler.java
@@ -5,6 +5,7 @@ public class TomcatPropsHandler {
 	private static String appurl = System.getProperty("appurl");
 
 	public static String getAppurl() {
+                System.out.println("getAppurl got invokede");
 		return appurl;
 	}
 
